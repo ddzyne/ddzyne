8068ad789eca1c75bf0bf418c514edae61aa90aa
Daan Janssen <daan@ddzyne.nl>
2019-11-07 13:39:29 +0100

    updated with inf scroll

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -4,33 +4,46 @@ import Isvg from 'react-inlinesvg'
 import { changeBackground } from '../actions'
 import { connect } from 'react-redux'
 import cn from 'classnames'
+import VisibilitySensor from 'react-visibility-sensor'
 
 class Home extends Component {
   constructor(props) {
     super(props)
     this.changeBackgroundClick = this.changeBackgroundClick.bind(this)
+    this.state = {
+      visible: false,
+    }
+    this.onChangeVisibility = this.onChangeVisibility.bind(this)
   }
   changeBackgroundClick(color) {
     this.props.dispatch(changeBackground(color))
   }
+  onChangeVisibility (isVisible) {
+    this.setState({visible: isVisible});
+  }
   render() {
     const { background } = this.props
     return (
-      <div className="wrapper home">
-        <div className="item leftside" id="logo">
-          <Isvg src={logo}>
-            Ddzyne - bespoke webdevelopment & design
-          </Isvg>
-        </div>
-        <div className="item rightside colorpicker">
-          <h4>Hoe voel je je vandaag?</h4>
-          <div className="big-wrap">
-            <Button text="Black is back" color="black" changeBackgroundClick={this.changeBackgroundClick} active={background === 'black'}/>
-            <Button text="Zeer sereen" color="white" changeBackgroundClick={this.changeBackgroundClick} active={background === 'white'}/>
-            <Button text="Vrolijk" color="pink" changeBackgroundClick={this.changeBackgroundClick} active={background === 'pink'}/>
+      <VisibilitySensor 
+        onChange={this.onChangeVisibility} 
+        active={!this.state.visible}
+        partialVisibility={true}>
+        <div className={`wrapper home ${this.state.visible ? 'animate-me' : ''}`}>
+          <div className="item leftside" id="logo">
+            <Isvg src={logo}>
+              Ddzyne - bespoke webdevelopment & design
+            </Isvg>
+          </div>
+          <div className="item rightside colorpicker">
+            <h4>Hoe voel je je vandaag?</h4>
+            <div className="big-wrap">
+              <Button text="Black is back" color="black" changeBackgroundClick={this.changeBackgroundClick} active={background === 'black'}/>
+              <Button text="Zeer sereen" color="white" changeBackgroundClick={this.changeBackgroundClick} active={background === 'white'}/>
+              <Button text="Vrolijk" color="pink" changeBackgroundClick={this.changeBackgroundClick} active={background === 'pink'}/>
+            </div>
           </div>
         </div>
-      </div>
+      </VisibilitySensor>
     );
   }
 }
