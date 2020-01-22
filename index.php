c7e21c860561316652249a0f5a2508794e1d6d0b
Daan Janssen <daan@ddzyne.nl>
2020-01-22 12:39:25 +0100

    wp fix

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
 <?php
-function wpse251841_wp_head() {
-    ob_start();
-    wp_head();
-    return ob_get_clean();
-}
-
-function wpse251841_wp_foot() {
-    ob_start();
-    wp_admin_bar_render();
-    return ob_get_clean();
-}
-
-ob_start();
 include("build/index.html"); 
-$contents = ob_get_contents();
-ob_end_clean();
-
-if ( current_user_can('administrator') ||  current_user_can('editor') ) {
-  $adminheader = str_replace('</head>', wpse251841_wp_head().'</head>', $contents);
-  $adminpage = str_replace('</body>', wpse251841_wp_foot().'</body>', $adminheader);
-  echo $adminpage;
-}
-else {
-  echo $contents;
-}
+?>
--- wp-library/over-template.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Template Name: Over
- */
-include_once('../build/index.html');
-
-?>
